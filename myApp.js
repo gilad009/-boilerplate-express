4fc1be6bf2c1ba42fdc3eb55605250ce65688d9e
gilad009 <gilad.milashut@gmail.com>
2024-09-26 16:00:08 +0200

    adding

--- myApp.js
+++ myApp.js
@@ -4,10 +4,9 @@ let app = express();
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-    res.send('Hello Express');
-  });
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-  let absolutePath = __dirname + '/views/index.html';
 
 
 module.exports = app;
