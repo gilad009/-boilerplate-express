80c5660c11be5abfdfd16c51eb9df0366dcd5d2b
gilad009 <gilad.milashut@gmail.com>
2024-09-26 16:24:29 +0200

    add3

--- myApp.js
+++ myApp.js
@@ -3,10 +3,12 @@ let app = express();
 
 console.log("Hello World");
 
+// Serve static files from the 'public' directory to the '/public' path
+app.use('/public', express.static(__dirname + '/public'));
+
+// Serve the index.html file for the root path
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-
-
 
 module.exports = app;
